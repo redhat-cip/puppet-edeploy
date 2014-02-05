8af4da7b1ce80209d05ec1aed234d4662a97e640
Michael Chapman <woppin@gmail.com>
2014-02-05 12:14:45 +0000

    Remove nosetests package
    This package isn't in any standard repos and doens't seem to
    be needed for most operations

--- manifests/params.pp
+++ manifests/params.pp
@@ -27,7 +27,7 @@ class edeploy::params {
       $packages = ['python-mock', 'python-netaddr', 'python-ipaddr', 'pigz', 'debootstrap', 'qemu-kvm', 'git']
     }
     'Debian': {
-      $packages = ['python-mock', 'python-netaddr', 'python-ipaddr', 'pigz', 'debootstrap', 'qemu-kvm', 'yum', 'qemu-utils', 'alien', 'python-openstack.nose-plugin', 'git']
+      $packages = ['python-mock', 'python-netaddr', 'python-ipaddr', 'pigz', 'debootstrap', 'qemu-kvm', 'yum', 'qemu-utils', 'alien', 'git']
     }
     default: {
       fail("Unsupported OS : ${::osfamily}")
